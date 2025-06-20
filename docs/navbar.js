f2590600a8a81f1de0a75903cc1da1543d1d97d3
Loup-Garou-MC <loup.garou.minecraft25@gmail.com>
2025-06-20 15:54:51 +0200

    Add files via upload

---
+++ docs/navbar.js
@@ -0,0 +1,17 @@
+
+document.addEventListener("DOMContentLoaded", () => {
+    const navbar = document.createElement("div");
+    const currentPage = window.location.pathname;
+    const match = currentPage.match(/\/([^\/]+)\/?$/);
+
+    navbar.className = "navbar";
+    navbar.innerHTML = `
+        <a ${!match[1] || match[1] === "docs" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "" : "../"}">Accueil</a>
+        <a ${match[1] === "ressources" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./ressources" : "../ressources"}">Ressources</a>
+        <a ${match[1] === "wiki" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./wiki" : "../wiki"}">WiKi</a>
+        <a ${match[1] === "parties" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./parties" : "../parties"}">Prochaines parties</a>
+        <a ${match[1] === "discord" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./discord" : "../discord"}">Discord</a>
+    `;
+
+    document.body.insertBefore(navbar, document.body.firstChild);
+});
