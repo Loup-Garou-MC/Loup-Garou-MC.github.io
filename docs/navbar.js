0c43342d148b6a0c1d6befc43f5a11e3b07ff673
Loup-Garou-MC <loup.garou.minecraft25@gmail.com>
2025-06-23 16:26:30 +0200

    Add files via upload

--- docs/navbar.js
+++ docs/navbar.js
@@ -4,8 +4,6 @@ document.addEventListener("DOMContentLoaded", () => {
     const currentPage = window.location.pathname;
     const match = currentPage.match(/\/([^\/]+)\/?$/);
 
-    console.log(currentPage, match[1])
-
     navbar.className = "navbar";
     navbar.innerHTML = `
         <a ${!match || (match && match[1] === "docs") ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "" : "../"}">Accueil</a>
