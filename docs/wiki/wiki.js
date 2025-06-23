f2c93af651b204c1506f915d11b47fce20d71f93
MiniTanjiro <143358912+MiniTanjiro@users.noreply.github.com>
2025-06-23 22:08:46 +0200

    Update wiki.js

--- docs/wiki/wiki.js
+++ docs/wiki/wiki.js
@@ -35,6 +35,20 @@ subItems.forEach(sub => {
     });
 });
 
+const svgRedirect = `
+    <svg xmlns="http://www.w3.org/2000/svg" 
+        width="12" height="12" 
+        fill="none" stroke="currentColor" stroke-width="2" 
+        stroke-linecap="round" stroke-linejoin="round" 
+        style="margin-left:4px; vertical-align:middle;">
+        <path d="M5 12h7M12 5v7M9 3h6v6"/>
+    </svg>
+`;
+
+document.querySelectorAll('a').forEach(a => {
+    a.insertAdjacentHTML('beforeend', svgRedirect);
+});
+
 
 window.addEventListener('DOMContentLoaded', () => {
     const hash = window.location.hash;
