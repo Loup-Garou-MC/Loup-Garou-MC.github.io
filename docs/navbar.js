b9afdd369ab2738ea14f4361d447bf85a407aac1
Loup-Garou-MC <loup.garou.minecraft25@gmail.com>
2025-06-25 13:35:14 +0200

    Update navbar.js

--- docs/navbar.js
+++ docs/navbar.js
@@ -1,16 +1,16 @@
-
 document.addEventListener("DOMContentLoaded", () => {
     const navbar = document.createElement("div");
     const currentPage = window.location.pathname;
     const match = currentPage.match(/\/([^\/]+)\/?$/);
+    const page = match ? match[1] : "";
 
     navbar.className = "navbar";
     navbar.innerHTML = `
-        <a ${!match || (match && match[1] === "docs") ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "" : "../"}">Accueil</a>
-        <a ${match[1] === "ressources" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./ressources" : "../ressources"}">Ressources</a>
-        <a ${match[1] === "wiki" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./wiki" : "../wiki"}">WiKi</a>
-        <a ${match[1] === "parties" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./parties" : "../parties"}">Prochaines parties</a>
-        <a ${match[1] === "discord" ? "class='actual'" : ""} href="${!match[1] || match[1] === "docs" ? "./discord" : "../discord"}">Discord</a>
+        <a ${page === "" || page === "docs" ? "class='actual'" : ""} href="${page === "" || page === "docs" ? "" : "../"}">Accueil</a>
+        <a ${page === "ressources" ? "class='actual'" : ""} href="${page === "" || page === "docs" ? "./ressources" : "../ressources"}">Ressources</a>
+        <a ${page === "wiki" ? "class='actual'" : ""} href="${page === "" || page === "docs" ? "./wiki" : "../wiki"}">WiKi</a>
+        <a ${page === "parties" ? "class='actual'" : ""} href="${page === "" || page === "docs" ? "./parties" : "../parties"}">Prochaines parties</a>
+        <a ${page === "discord" ? "class='actual'" : ""} href="${page === "" || page === "docs" ? "./discord" : "../discord"}">Discord</a>
     `;
 
     document.body.insertBefore(navbar, document.body.firstChild);
